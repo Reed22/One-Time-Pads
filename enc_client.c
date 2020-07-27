cfb878384ca2c3e56128756cd80669a6185db5e5
Reed Robinson <robinree@os1.engr.oregonstate.edu>
2020-07-27 08:09:44 -0700

    client can send multiple messages to server, server sends ciphertext back. Encryption not working with spaces.

--- enc_client.c
+++ enc_client.c
@@ -16,7 +16,7 @@
 #define Z_ASCII 90    //90 is ascii for 'Z'
 #define SPACE_ASCII 32  //Ascii for space character
 
-bool debug = true;
+bool debug = false;
 /**************************************************************
 *                 void error(const char *msg)
 ***************************************************************/
@@ -59,12 +59,12 @@ void setupAddressStruct(struct sockaddr_in* address,
 /**************************************************************
 *                 cchar* readFile(char* file_name)
 ***************************************************************/
-char* readFile(char* file_name){
+char* readFile(int socket, char* file_name, int length){
   	//int file_descriptor;
 	  char file_path[256];
     char c;
     char* readBuffer;
-    int length; 
+    //int length; 
     FILE* file;
 
     file = fopen(file_name, "r");
@@ -88,19 +88,76 @@ char* readFile(char* file_name){
     }
     //Strip the newline character
     buf[length - 1] = '\0';
-    readBuffer = buf;
     fclose(file);
     return readBuffer;
 }
 
 /**************************************************************
-*                  checkLen(plaintext, key)
+*                 char* sendFile(char* file_name)
 ***************************************************************/
-bool checkLen(char* plain, char* key){
-      size_t len_plain = strlen(plain);
-      printf("%d\n", strlen(plain));
-      return true;
+char* sendFile(int socket, char* file_name, int length){
+  	//int file_descriptor;
+	  char file_path[256];
+    char c;
+    FILE* file;
+    char buf[length];
+    int chars_written;
+
+    file = fopen(file_name, "r");
+
+    fseek (file, 0, SEEK_SET);
+
+    for(int i = 0; i < length; i++){
+      c = getc(file);
+      if(!isupper(c) && !isspace(c)){
+        fprintf(stderr, "Error: Bad character detected\n");
+        return NULL;
+      }
+      buf[i] = c;
+    }
+    //Strip the newline character
+    buf[length] = '\0';
+    
+    if(debug){
+      printf("buf: %s\n", buf);
+    }
+
+    fclose(file);
+
+    chars_written = send(socket, buf, strlen(buf), 0); 
+
+    if (chars_written < 0){
+      error("CLIENT: ERROR writing to socket");
+    }
+    
+    if (chars_written < strlen(buf)){
+      printf("CLIENT: WARNING: Not all data written to socket!\n");
+    }
+    return NULL;
 }
+
+
+/**************************************************************
+*                 int fileLength(char* file_name)
+***************************************************************/
+int fileLength(char* file_name){
+  	//int file_descriptor;
+	  char file_path[256];
+    char c;
+    char* readBuffer;
+    int length; 
+    FILE* file;
+
+    file = fopen(file_name, "r");
+
+    // Determine how many characters are in the file
+    fseek (file, 0, SEEK_END);
+    length = ftell(file);
+
+    //return length - 1 to exclude newline character from count
+    return length - 1;
+}
+
 /**************************************************************
 *                 int main(int argc, char *argv[])
 ***************************************************************/
@@ -115,32 +172,21 @@ int main(int argc, char *argv[]) {
     fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]); 
     exit(0); 
   } 
+  //Check to make sure key is at least as big as plaintext
+  //argv[1] = plaintext    argv[2] = key
+  int plaintext_len = fileLength(argv[1]);
+  int key_len = fileLength(argv[2]);
 
-  //Read plaintext file
-  char* plaintext = readFile(argv[1]);
-  if(plaintext == NULL){
-    exit(1);
-  }
-  else{
-    if(debug)
-      printf("plaintext : %s length %d\n", plaintext, strlen(plaintext));
-  }
-
-  //Read key file
-  char* key = readFile(argv[2]);
-  if(key == NULL){
-    exit(1);
-  }
-  else{
-    if(debug)
-      printf("key: %s length %d\n", key, strlen(key));
-  }
-
-  //Confirm that key is at least as long as plaintext
-  if(strlen(key) < strlen(plaintext)){
+  if(fileLength(argv[1]) > fileLength(argv[2])){
     fprintf(stderr,"Error: Key is shorter than plaintext\n");
     exit(1);
   }
+
+  if(debug){
+    printf("Plaintext LEN: %d\n", plaintext_len);
+    printf("Key Len: %d\n", key_len);
+  }
+
   // Create a socket
   socketFD = socket(AF_INET, SOCK_STREAM, 0); 
   if (socketFD < 0){
@@ -155,28 +201,33 @@ int main(int argc, char *argv[]) {
   if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
     error("CLIENT: ERROR connecting");
   }
+  //Send plaintext
+  sendFile(socketFD, argv[1], plaintext_len);
+  sendFile(socketFD, argv[2], key_len);
+
+
   // Get input message from user
-  printf("CLIENT: Enter text to send to the server, and then hit enter: ");
+  //printf("CLIENT: Enter text to send to the server, and then hit enter: ");
   // Clear out the buffer array
-  memset(buffer, '\0', sizeof(buffer));
+  //memset(buffer, '\0', sizeof(buffer));
   // Get input from the user, trunc to buffer - 1 chars, leaving \0
-  fgets(buffer, sizeof(buffer) - 1, stdin);
+  //fgets(buffer, sizeof(buffer) - 1, stdin);
   // Remove the trailing \n that fgets adds
-  buffer[strcspn(buffer, "\n")] = '\0'; 
+  //buffer[strcspn(buffer, "\n")] = '\0'; 
 
   // Send message to server
   // Write to the server
-  charsWritten = send(socketFD, buffer, strlen(buffer), 0); 
-  if (charsWritten < 0){
-    error("CLIENT: ERROR writing to socket");
-  }
-  if (charsWritten < strlen(buffer)){
-    printf("CLIENT: WARNING: Not all data written to socket!\n");
-  }
+  //charsWritten = send(socketFD, buffer, strlen(buffer), 0); 
+  //if (charsWritten < 0){
+  //  error("CLIENT: ERROR writing to socket");
+  //}
+  //if (charsWritten < strlen(buffer)){
+  //  printf("CLIENT: WARNING: Not all data written to socket!\n");
+  //}
 
   // Get return message from server
   // Clear out the buffer again for reuse
-  memset(buffer, '\0', sizeof(buffer));
+  //memset(buffer, '\0', sizeof(buffer));
   // Read data from the socket, leaving \0 at end
   charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); 
   if (charsRead < 0){
--- enc_server.c
+++ enc_server.c
@@ -6,13 +6,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-// Error function used for reporting issues
+#define NUM_MESSAGES 2
+#define PLAIN_MSG 0
+#define KEY_MSG 1
+#define SPACE_ASCII 32
+
+/**************************************************************
+*                 void error(const char *msg)
+***************************************************************/
 void error(const char *msg) {
   perror(msg);
   exit(1);
 } 
 
-// Set up the address struct for the server socket
+/**************************************************************
+* void setupAddressStruct(struct sockaddr_in* address, 
+*                         int portNumber)
+***************************************************************/
 void setupAddressStruct(struct sockaddr_in* address, 
                         int portNumber){
  
@@ -26,12 +36,55 @@ void setupAddressStruct(struct sockaddr_in* address,
   // Allow a client at any address to connect to this server
   address->sin_addr.s_addr = INADDR_ANY;
 }
+/**************************************************************
+*                 void encrypt(plain, key)
+***************************************************************/
+void encrypt(int conn_socket, char plain[], char key[]){
+    int plain_len = strlen(plain);
+    int plain_num, key_num, charsRead;
+    char ciphertext[plain_len];
 
+    //plain_ascii = 'A' - 65;
+    //printf("PLAIN ASCII: %d\n", plain_ascii);
+
+    for(int i = 0; i < plain_len; i++){
+        //Subtract 65 from ASCII to get order (A = 0, Z = 25)
+        plain_num = plain[i] - 65;
+        key_num = key[i] - 65;
+
+        //If character is space, make the number 26 (one after Z)
+        //Doing this after assigning nums to change num if the actual
+        //character is a space
+        if(plain[i] == SPACE_ASCII)
+          plain_num = 26;
+
+        if(key[i] == SPACE_ASCII)
+          key_num = 26;
+        
+        //Mod 27 to account for space characters....need confirmation that this is okay. 
+        //Add plain_num and key_num
+        //Take Modulus of 27
+        //Add 65 to get ASCII back
+        ciphertext[i] = ((plain_num + key_num) % 27) + 65;
+
+        if(ciphertext[i] == 26 + 65)
+          ciphertext[i] == SPACE_ASCII;
+    }
+
+    charsRead = send(conn_socket, ciphertext, strlen(ciphertext), 0); 
+    if (charsRead < 0)
+      error("ERROR writing to socket");
+}
+/**************************************************************
+*              int main(int argc, char *argv[])
+***************************************************************/
 int main(int argc, char *argv[]){
   int connectionSocket, charsRead;
-  char buffer[256];
+  char plain_buf[256];
+  char key_buf[256];
   struct sockaddr_in serverAddress, clientAddress;
   socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
 
   // Check usage & args
   if (argc < 2) { 
@@ -73,14 +126,32 @@ int main(int argc, char *argv[]){
                           ntohs(clientAddress.sin_port));
 
     // Get the message from the client and display it
-    memset(buffer, '\0', 256);
-    // Read the client's message from the socket
-    charsRead = recv(connectionSocket, buffer, 255, 0); 
-    if (charsRead < 0){
-      error("ERROR reading from socket");
-    }
-    printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+    memset(plain_buf, '\0', 256);
+    memset(key_buf, '\0', 256);
+    for(int i = 0; i < NUM_MESSAGES; i++){
+        // Read the client's message from the socket
+        //First message is plaintext;
+        if(i == PLAIN_MSG){
+          charsRead = recv(connectionSocket, plain_buf, 255, 0); 
+          if (charsRead < 0){
+            error("ERROR reading from socket");
+          }
+          printf("SERVER: I received this from the client: \"%s\"\n", plain_buf);
+        }
 
+        //Second message is key
+        else if(i == KEY_MSG){
+          charsRead = recv(connectionSocket, key_buf, 255, 0); 
+          if (charsRead < 0){
+            error("ERROR reading from socket");
+          }
+          printf("SERVER: I received this from the client: \"%s\"\n", key_buf);
+        }
+    }
+
+    //Encrypt plaintext and send
+    encrypt(connectionSocket, plain_buf, key_buf);
+/*
     // Send a Success message back to the client
     charsRead = send(connectionSocket, 
                     "I am the server, and I got your message", 39, 0); 
@@ -88,6 +159,7 @@ int main(int argc, char *argv[]){
       error("ERROR writing to socket");
     }
     // Close the connection socket for this client
+*/
     close(connectionSocket); 
   }
   // Close the listening socket
